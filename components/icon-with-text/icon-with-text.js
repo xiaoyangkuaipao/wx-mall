5cb888f64dbbbca306f9120013ea847dda8ae06a
iamyangqi@sina.com <iamyangqi@sina.com>
2018-02-09 09:45:33 +0800

    first commit

---
+++ components/icon-with-text/icon-with-text.js
@@ -0,0 +1,26 @@
+// components/icon-with-text.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    "postData": {
+      type: Object,
+      value: {}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
